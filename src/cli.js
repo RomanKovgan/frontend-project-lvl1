d9b0096b0b1467dccd1edb0844f75e08cf81b8a0
RomanKovgan <r.kovgan88@gmail.com>
2022-03-18 11:41:52 +0300

    finished part three

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+export default function sayHello() {
+    const userName = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + userName + '!');
+};
