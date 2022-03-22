807d0f6451cd5e01aca11e2205ec8bea3182b43e
RomanKovgan <r.kovgan88@gmail.com>
2022-03-22 18:07:16 +0300

    finish step five

---
+++ bin/brain-even.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+let userName = '';
+function sayHello() {
+  userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+}
+
+function answered() {
+  const randomNumber = Math.floor(Math.random() * 100);
+  const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+  console.log(`Question: ${randomNumber}`);
+  const userAnswer = readlineSync.question('Your answer:');
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
+  return false;
+}
+function startGame() {
+  console.log('Welcome to the Brain Games!');
+  sayHello();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let count = 0;
+  while (count < 3) {
+    const isRight = answered();
+    if (!isRight) {
+      return;
+    }
+    count += 1;
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+}
+
+startGame();
